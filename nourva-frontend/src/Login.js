a559e729031126e3b710c7133024d46d5750858a
RainbowPigCake <kaitlynwong97@gmail.com>
2023-07-15 01:26:58 -0400

    integrated html and css files into react #6

---
+++ nourva-frontend/src/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Login.css'
+
+export default function Login() {
+    return (
+        <>
+            <div class="container">
+                <h2>Login</h2>
+                <form>
+                    <input type="text" name="username" placeholder="Username" required />
+                    <input type="password" name="password" placeholder="Password" required />
+                    <button type="submit" value="Login">Login</button>
+                </form>
+
+            </div>
+            <div class="container2">
+                <label>Don't have an account? &nbsp</label>
+                <a href="https://www.google.com/"> Sign Up</a>
+            </div>
+        </>
+    )
+}
