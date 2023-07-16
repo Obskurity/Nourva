9206dfcdcac26c083ad0929b6c26c76b08b973d7
sooriyaradhakrishnan <sradh072@uottawa.ca>
2023-07-15 20:51:52 -0400

    entry code for signup (removed post method from form) #6

--- nourva-frontend/src/Signup.js
+++ nourva-frontend/src/Signup.js
@@ -1,13 +1,48 @@
-const Signup = () => {
-    const subtitle = "Signup noob!"
+import './Login.css'
+import { useState, useRef } from 'react'
+
+
+export default function Signup() {
+    const password1 = useRef()
+    const password2 = useRef()
+    const btnSubmit = useRef()
+    const notMatching = useRef()
+    function checkPass(e) {
+
+        const p1 = password1.current.value
+        const p2 = password2.current.value
+    
+        if (p1 !== p2){
+            notMatching.current.value = "Passwords do not match!"
+        }
+        else{
+
+        }
+        
+    }
     return (
-        <div>
-            <div className = 'content'>
-                <h1>{ subtitle }</h1>
+        <>
+
+            <div id="userInfo" class="container">
+                <h2>Sign Up</h2>
+                <form>
+                    <input  type="text" name="username" placeholder="Username" required />
+                    <br></br>
+                    <input ref={password1} type="password" name="password1" placeholder="Password" required />
+                    <br></br>
+                    <input ref={password2} type="password" name="password2" placeholder="Confirm Password" required />
+                    <br></br>
+                    <button ref={btnSubmit} id="btnSubmit"  value="Login" onClick={checkPass}>Create Account</button>
+                    <br></br>
+                    <label ref={notMatching}></label>
+                </form>
             </div>
+            <br></br>
+            <div class="container2">
+                <label>Already have an account? &nbsp;</label>
+                <a href="/login">Login</a>
+            </div>
+        </>
 
-        </div>
-    );
-};
-
-export default Signup;
+    )
+}
