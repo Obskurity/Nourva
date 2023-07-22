3959928120020fb77ea4d156e58ef3ed2ecbdab0
unknown <ctrlaltdeleteyou@gmail.com>
2023-07-22 01:01:26 -0400

    finished register screen and jwt auth#17

--- nourva-frontend/src/NutrientTrackingComponents/DailyFoods.js
+++ nourva-frontend/src/NutrientTrackingComponents/DailyFoods.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
+import moment from 'moment';
+import { DatePicker } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers';
 
 const DailyFoods = () => {
     // Add logic to fetch and display the list of foods the user has eaten for the day.
+    const dateFormatString = "dddd MMMM D";
+    const [pickedDate, setPickedDate] = useState(moment())
+    const [showDatePicker, setShowDatePicker] = useState(false);
+
+    const handleDatePickerClick = () => {
+        setShowDatePicker(!showDatePicker);
+    };
+
+    const handleDateChange = (newDate) => {
+        setShowDatePicker(false);
+        if (newDate.isValid()) {
+            setPickedDate(newDate);
+        }
+    };
+
+    const handlePreviousDate = () => {
+        setPickedDate(pickedDate.subtract(1, 'day'));
+    };
+
+    const handleNextDate = () => {
+        setPickedDate(pickedDate.add(1, 'day'));
+    };
+
     return (
         <div>
-            <h2>Monday, January 1</h2>
+            <button onClick={handlePreviousDate}>Previous</button>
+            <h2 onClick={handleDatePickerClick}>{pickedDate.format(dateFormatString)}</h2>
+            <button onClick={handleNextDate}>Next</button>
+            {showDatePicker && (
+                <div>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            onChange={handleDateChange}
+                        />
+                    </LocalizationProvider>
+                </div>
+            )}
             <ul>
                 {/* Render the list of foods the user has eaten for the day */}
                 {/* Each item should show the food name and its calories */}
--- nourva-frontend/src/NutrientTrackingComponents/NutrientTracking.js
+++ nourva-frontend/src/NutrientTrackingComponents/NutrientTracking.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import CalorieProgressBar from './CalorieProgressBar'
-import DatePicker from './DatePicker'
 import Header from './Header'
 import QueryFood from './DailyFoods'
 import AddFoodPopup from './AddFoodPopup'
@@ -8,10 +7,12 @@ import AddFoodPopup from './AddFoodPopup'
 export default function NutrientTracking() {
     const [showPopup, setShowPopup] = useState(false);
 
-    const handleAddFood = (foodName, measurement) => {
+
+    const handleAddFood = (foodName, measurement, calories) => {
         // Handle the food addition logic here (e.g., API calls or updating state)
         console.log('Food Name:', foodName);
         console.log('Measurement:', measurement);
+        console.log('Calories:', calories);
     };
 
     return (
@@ -20,7 +21,6 @@ export default function NutrientTracking() {
             <div style={{ display: 'flex' }}>
                 <QueryFood />
                 <div>
-                    <DatePicker />
                     <CalorieProgressBar goal={2000} consumed={1500} />
                 </div>
             </div>
