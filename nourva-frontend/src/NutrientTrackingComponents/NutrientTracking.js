f7e295c052e4464881f1007e81da20577ee1d4da
RainbowPigCake <kaitlynwong97@gmail.com>
2023-07-22 00:48:58 -0400

    add date picker #10

---
+++ nourva-frontend/src/NutrientTrackingComponents/NutrientTracking.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import CalorieProgressBar from './CalorieProgressBar'
+import Header from './Header'
+import QueryFood from './DailyFoods'
+import AddFoodPopup from './AddFoodPopup'
+
+export default function NutrientTracking() {
+    const [showPopup, setShowPopup] = useState(false);
+
+
+    const handleAddFood = (foodName, measurement, calories) => {
+        // Handle the food addition logic here (e.g., API calls or updating state)
+        console.log('Food Name:', foodName);
+        console.log('Measurement:', measurement);
+        console.log('Calories:', calories);
+    };
+
+    return (
+        <div>
+            <Header onAddFood={() => setShowPopup(true)} />
+            <div style={{ display: 'flex' }}>
+                <QueryFood />
+                <div>
+                    <CalorieProgressBar goal={2000} consumed={1500} />
+                </div>
+            </div>
+            {showPopup && <AddFoodPopup onClose={() => setShowPopup(false)} onAddFood={handleAddFood} />}
+        </div>
+    )
+}
